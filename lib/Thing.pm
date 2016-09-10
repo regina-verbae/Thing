d6aa58b3a11afdd37203969793e0205bd325d41f
Mary Ehlers <regina.verbae@gmail.com>
2016-09-10 07:40:35 -0400

    The Thing library

---
+++ lib/Thing.pm
@@ -0,0 +1,37 @@
+#####################################################################
+## AUTHOR: Mary Ehlers, regina.verbae@gmail.com
+## ABSTRACT: Just a thing
+#####################################################################
+
+package Thing;
+
+use v5.10;
+use warnings;
+
+use Path::Tiny;
+
+#####################################################################
+
+=head1 SUBROUTINES NOT AVAILABLE FOR EXPORT
+
+=head2 do_thing
+
+It's really dumb!
+
+=cut
+
+sub do_thing {
+    say 'THING!';
+}
+
+#####################################################################
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+    Piper
+
+=cut
